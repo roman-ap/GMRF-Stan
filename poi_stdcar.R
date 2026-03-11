b380089a2091c4d668299cfdb86550899bc2fe15
Roman Aguirre-Perez <roman@sigma.iimas.unam.mx>
2026-03-11 13:46:52 -0600

    Improving script.

--- poi_stdcar.R
+++ poi_stdcar.R
@@ -1,22 +1,29 @@
+##### Poisson regression with CAR effects 
+##### using Sweden's counties as irregular lattice 
+
+### Set-up
 library(sf)
 library(dplyr)
 library(Matrix)
 library(cmdstanr)
-library(posterior)
-library(bayesplot)
 library(extraDistr)
 library(withr)
 library(ggplot2)
 library(latex2exp)
 
-
-
-counties <- read_sf("/home/roman/Nextcloud/Research/WildBoar/Data/Inputs/counties.shp")
-counties <- counties[-8,]
+### Sweden's counties
+# Spatial geometries
+counties <- read_sf("./Sweden/counties.shp")
+counties <- counties[-8,] #Removing Gotland
 plot(counties)
 
+### Spatial structure
+## CAR(1) structure
+# Number of counties
 S<-nrow(counties)
+# Binary spatial relationships  
 S_match<-st_relate(counties,counties,pattern="****11212",sparse=TRUE)
+# Sparse matrices
 Ds_ii<-sapply(S_match,length)
 D<-sparseMatrix(i=c( 1:S ),
                 j=c( 1:S ),
@@ -30,15 +37,18 @@ C<-sparseMatrix(i=c( rep(1:S, Ds_ii) ),
                 j=c( unlist(S_match) ),
                 x=c( rep(1/Ds_ii, Ds_ii) ),
                 dims=c(S,S))
+# Eigenvalues of C_space
 lambdas<-Schur(C, vectors = FALSE)$EValues
 
-pm_mu = 6
+### Realisation of MARCAR effects
+## Hyperpriors for the mean, standard deviation and spatial dependence
+pm_mu = 1
 psd_mu = 0.1
 pm_sigma = 0
 psd_sigma = 0.1
-pm_rho = 0
-psd_rho = 0.3
-### Realisation
+pm_rho = 0.5
+psd_rho = 0.1
+## Realisation
 with_seed(87L,{
   mu_obs <- rnorm(n = 1, pm_mu, psd_mu)
   sigma_obs <- rtnorm(n = 1, pm_sigma, psd_sigma, a=0)
@@ -52,8 +62,8 @@ with_seed(87L,{
   u = solve(L,u_per,system="Pt")
   eta = mu_obs + sigma_obs*u
 })
-
-#Pop <- c(2454821,404589,301944,472298,368856,203686,246667,157973,1421781,343746,1767016,283548,308116,280813,287253,285642,242148,132572,278729,248480)
+## Realization of a Poisson regression with rates exp(eta)
+## where eta follows a CAR distribution   
 y_obs <- numeric(S)
 mu_y <- numeric(S)
 for(s in 1:S){
@@ -61,17 +71,17 @@ for(s in 1:S){
   y_obs[s] = rpois(1, mu_y[s])
 }
 
+### Bayesian estimation via HMC (Stan)
+# Stan model using a sparse representation for the MARCAR distribution
+car_model <- cmdstan_model("./poi_stdcar.stan")
+# Data inputs for the Stan models
 A_w<-A@x
 A_v<-A@i +1
 A_u<-A@p +1
 log_det_D<-sum(log(Ds_ii))
-
-
-car <- cmdstan_model("./poi_stdcar.stan")
-
+# Stan inputs
 data_list <- list(S = S,
                   y = y_obs,
-                  #log_offset = log(Pop),
                   pm_mu = pm_mu,
                   psd_mu = psd_mu,
                   pm_sigma = pm_sigma,
@@ -85,27 +95,24 @@ data_list <- list(S = S,
                   A_w = A_w,
                   A_v = A_v,
                   A_u = A_u)
-
-car_fit <- car$sample(data = data_list,
-                      chains = 4,
-                      parallel_chains = 4,
-                      refresh = 100,
-                      iter_warmup = 1000,
-                      iter_sampling = 1000
+## MCMC sampling
+# Sampler
+car_fit <- car_model$sample(data = data_list,
+                            chains = 4,
+                            parallel_chains = 4,
+                            refresh = 100,
+                            iter_warmup = 1000,
+                            iter_sampling = 1000
 )
-
+# Posterior summaries
 car_fit$summary(variables = c("mu", "sigma", "rho"))
 
 
 
-##### Visualizations
-### Data frame with draws 
+### Visualizations (Prior and posterior distributions + observed value)
+## Data frame with posterior draws 
 car_draws <- car_fit$draws(format = "df")
-### Half normal probability density function
-dhnorm <- function(x, sigma) {
-  ifelse(x >= 0, (sqrt(2)/(sigma *sqrt(pi)))*exp(-x^2/(2*sigma^2)), 0)
-}
-#
+# Mean
 ggplot(data.frame(draw=car_draws$`mu`)) +
   geom_density(aes(x=draw, y=after_stat(density)), color="pink", fill="pink") +
   stat_function(fun = dnorm, args = list(mean=pm_mu, sd=psd_mu), colour="black", linewidth=1) +
@@ -115,7 +122,7 @@ ggplot(data.frame(draw=car_draws$`mu`)) +
   labs(title = TeX("Prior and posterior distribution of $\\mu$"),
        y = TeX("Density"), x = TeX("$\\mu$")) +
   theme(plot.title = element_text(size = 18, face = "bold", hjust = 0.5))
-
+# Standard deviation
 ggplot(data.frame(draw=car_draws$`sigma`)) +
   geom_density(aes(x=draw, y=after_stat(density)), color="pink", fill="pink") +
   stat_function(fun = dhnorm, args = list(sigma=psd_sigma), colour="black", linewidth=1) +
@@ -125,13 +132,11 @@ ggplot(data.frame(draw=car_draws$`sigma`)) +
   labs(title = TeX("Prior and posterior distribution of $\\sigma$"),
        y = TeX("Density"), x = TeX("$\\sigma$")) +
   theme(plot.title = element_text(size = 18, face = "bold", hjust = 0.5))
-
+# Spatial dependence
 ggplot(data.frame(draw=car_draws$`rho`)) +
   geom_density(aes(x=draw, y=after_stat(density)), color="pink", fill="pink") +
   stat_function(fun = dnorm, args = list(mean=pm_rho, sd=psd_rho), colour="black", linewidth=1) +
   xlim(pm_rho - 6*psd_rho, pm_rho + 6*psd_rho) +
-  #stat_function(fun = dunif, args = list(min=1/min(lambdas), max=1/max(lambdas)), colour="black", linewidth=1) +
-  #xlim(1/min(lambdas), 1/max(lambdas)) +
   geom_vline(xintercept = rho_obs, color="blue") +
   theme_minimal() +
   labs(title = TeX("Prior and posterior distribution of $\\rho$"),
